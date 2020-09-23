010b509dd479b2065fc925fa69afc8c067455c48
Vlad Novik <vlad.novik.mix@gmail.com>
2020-09-23 19:37:21 +0300

    Initial Commit

---
+++ TrafficEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficEditor
+//
+//  Created by Vlad Novik on 9/23/20.
+//  Copyright © 2020 Vlad Novik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
